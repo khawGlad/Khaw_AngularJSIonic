834d743aa363df200974f9bd21e1f36410a704c1
khawGlad <gladyskhaw3095@gmail.com>
2017-02-10 00:13:03 +0800

    my commit

--- myApp/www/js/app.js
+++ myApp/www/js/app.js
@@ -1,3 +1,4 @@
+
 // Ionic Starter App
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
@@ -7,15 +8,10 @@ angular.module('starter', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
     if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
     }
     if(window.StatusBar) {
       StatusBar.styleDefault();
@@ -23,7 +19,8 @@ angular.module('starter', ['ionic'])
   });
 })
 
-.config(function($stateProvider, urlRouterProvider) {
+
+.config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
     .state('tabs', {
       url: '/tab',
@@ -32,38 +29,39 @@ angular.module('starter', ['ionic'])
     })
     .state('tabs.list', {
       url: '/list',
-      view: {
+      views: {
         'list-tab' : {
           templateUrl: 'templates/list.html',
-          controller: "ListController"
+          controller: 'ListController'
         }
       }
     })
-    $urlRouterProvider.otherwise('/tab/list');
+  $urlRouterProvider.otherwise('/tab/list');
 })
 
+
 .controller('ListController', ['$scope', '$http', function($scope, $http) {
-  $http.get('js/data.json').success(function(data) {
-    $scope.artists = data;
+    $http.get('js/data.json').success(function(data) {
+      $scope.artists = data;  
 
-    $scope.onItemDelete = function(item) {
-      $scope.artists.splice($scope.artists.indexOf(item), 1);
-    }
+      $scope.onItemDelete = function(item) {
+        $scope.artists.splice($scope.artists.indexOf(item), 1);
+      }
 
-    $scope.doRefresh = function() {
+      $scope.doRefresh =function() {
       $http.get('js/data.json').success(function(data) {
-        $scope.artists = data;
-        $scope.$broadcast('scroll.refreshComplete');
-      });
-    }
+          $scope.artists = data;
+          $scope.$broadcast('scroll.refreshComplete'); 
+        });
+      }
 
-    $scope.toggleStar = function(item) {
-      item.star = !item.star;
-    }
+      $scope.toggleStar = function(item) {
+        item.star = !item.star;
+      }
 
-    $scope.moveItem = function(item, fromIndex, toIndex) {
-      $scope.artists.splice(fromIndex, 1);
-      $scope.artists.splice(toIndex, 0, item);
-    };
-  });
+      $scope.moveItem = function(item, fromIndex, toIndex) {
+        $scope.artists.splice(fromIndex, 1);
+        $scope.artists.splice(toIndex, 0, item);
+      };
+    });
 }]);
